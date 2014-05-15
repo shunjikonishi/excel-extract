c828a7f70bd3160488c65764a1b84998876fd4e2
Shunji Konishi <shun.konishi@nifty.com>
2014-05-15 22:21:34 +0900

    test

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,8 +5,18 @@ import play.api.mvc._
 
 object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def index = Action { implicit request =>
+    println("METHOD: " + request.method)
+    var ret = if (request.method == "OPTIONS") {
+      Ok("")
+    } else {
+      Ok("""{"status" : "OK"}""").as("application/json")
+    }
+    ret.withHeaders(
+      "Access-Control-Allow-Origin" -> "http://localhost:9000",
+      "Access-Control-Allow-Methods" -> "POST, GET, OPTIONS",
+      "Access-Control-Allow-Headers" -> "X-PINGOTHER"
+    )
   }
 
 }
