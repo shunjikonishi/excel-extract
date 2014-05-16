15f32fa61b1eae851258eb7e088c317f0daf8e60
k-shunji <k-shunji@flect.co.jp>
2014-05-16 15:16:48 +0900

    Excel Extract

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,12 +2,18 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import java.io.File
+import java.io.IOException
+import java.util.Locale
+import jp.co.flect.excel2canvas.NamedValueExtractor
 
 object Application extends Controller {
 
   def index = Action { implicit request =>
-    println("METHOD: " + request.method)
-    println("ORIGIN: " + request.headers)
+    Ok(views.html.index("Excel extractor"))
+    /*
     var ret = if (request.method == "OPTIONS") {
       Ok("")
     } else {
@@ -18,10 +24,31 @@ object Application extends Controller {
       "Access-Control-Allow-Methods" -> "POST, GET, OPTIONS",
       "Access-Control-Allow-Headers" -> "X-PINGOTHER"
     )
+    */
   }
 
-  def index2 = Action {
-    Ok(views.html.index("test"))
+  def upload = Action(parse.multipartFormData) { implicit request =>
+    val (filename, file) = request.body.file("file").map { file =>
+      val temp = File.createTempFile("tmp", file.filename)
+      file.ref.moveTo(temp, true)
+      (file.filename, temp)
+    }.getOrElse(throw new IOException("Could not find uploaded file."))
+    Form(tuple(
+      "locale" -> nonEmptyText,
+      "convertString" -> boolean,
+      "includeFormula" -> boolean
+    )).bindFromRequest.fold(
+      hasErrors={ form =>
+        throw new IllegalStateException()
+      },
+      success={ case (locale, convertString, includeFormula) =>
+        val extractor = new NamedValueExtractor(new Locale(locale))
+        extractor.setConvertString(convertString)
+        extractor.setIncludeFormulaValue(includeFormula)
+        val map = extractor.extract(file)
+        Ok(NamedValueExtractor.toJson(map)).as("application/json;charset=utf-8")
+      }
+    )
   }
 
 }
--- build.sbt
+++ build.sbt
@@ -3,9 +3,10 @@ name := "excel-extract"
 version := "1.0-SNAPSHOT"
 
 libraryDependencies ++= Seq(
-  jdbc,
-  anorm,
-  cache
+  "org.apache.poi" % "poi" % "3.10-FINAL",
+  "org.apache.poi" % "poi-ooxml" % "3.10-FINAL",
+  "org.apache.poi" % "ooxml-schemas" % "1.1",
+  "com.google.code.gson" % "gson" % "2.2.4"
 )     
 
 play.Project.playScalaSettings
